83d6fd18e67ad52f560d44fd20b2654cd146aae3
reneenordholm <47894926+reneenordholm@users.noreply.github.com>
2019-06-05 14:26:04 -0500

    add files via upload

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in horoscope_reader.gemspec
+gemspec
---
+++ lib/horoscope_reader.rb
@@ -0,0 +1,6 @@
+require "horoscope_reader/version"
+
+module HoroscopeReader
+  class Error < StandardError; end
+  # Your code goes here...
+end
